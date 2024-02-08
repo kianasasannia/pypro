55a5694e20f42828bd2d9557f16e08e81aa7fbb3
kiana <kianasasannia@gmail.com>
2024-02-08 06:57:05 +0330

    new version

--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pages/',include('pages.urls',namespace='pages'))
+    #path('pages/',include('pages.urls',namespace='pages'))
+    path('',include('pages.urls')),
     
 ]
